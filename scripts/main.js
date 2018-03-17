2fde76360a087f25b603c6488cc4e9601fcc2083
Tor020 <kopp020@gmail.com>
2018-03-17 17:25:58 -0400

    'updates'

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-
+"use strict";discordWidget.init({serverId:"364473157312315394",title:"Devvit.io",join:!1,alphabetical:!0,theme:"light",hideChannels:!0,showAllUsers:!0,allUsersDefaultState:!1}),discordWidget.render(),function(e,t){t.getElementById("cb1"),t.getElementById("cb2");function i(e){}t.querySelectorAll("input[type=checkbox]").forEach(function(e){return e.addEventListener("change",i)})}(window,document);
