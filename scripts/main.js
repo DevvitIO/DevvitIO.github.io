9c6678530c5761d6ae50178cd96f1aa26c8f77c2
Tor020 <kopp020@gmail.com>
2018-03-19 11:29:20 -0400

    Better Responsiveness Implemented by KenavR

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,43 @@
-"use strict";discordWidget.init({serverId:"364473157312315394",title:"Devvit.io",join:!1,alphabetical:!0,theme:"light",hideChannels:!0,showAllUsers:!0,allUsersDefaultState:!1}),discordWidget.render(),function(e,t){t.getElementById("cb1"),t.getElementById("cb2");function i(e){}t.querySelectorAll("input[type=checkbox]").forEach(function(e){return e.addEventListener("change",i)})}(window,document);
+
+
+
+      discordWidget.init({
+          serverId: '364473157312315394',
+          title: 'Devvit.io',
+          join: false,
+          alphabetical: true,
+          theme: 'light',
+          hideChannels: true,
+          showAllUsers: true,
+          allUsersDefaultState: false
+      });
+      discordWidget.render();
+
+
+(function (window, document) {
+
+const cb1 = document.getElementById('cb1');
+const cb2 = document.getElementById('cb2');
+
+const events = document.querySelectorAll('input[type=checkbox]');
+
+// events.addEventListener('change', bothChecked);
+events.forEach(cb => cb.addEventListener('change', bothChecked));
+
+
+function bothChecked(e){
+}
+
+
+
+
+  
+})(window, document);
+
+
+
+
+
+
+
+
